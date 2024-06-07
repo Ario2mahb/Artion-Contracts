ed78809ceab92dd4b40654fff5e7d879311bec0a
Ario2mahb <138014505+Ario2mahb@users.noreply.github.com>
2024-06-07 15:50:05 +0330

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,7 +12,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: {
-    version: '0.6.12',
+    version: '9.7.0',
     settings: {
       optimizer: {
         enabled: true,
